a228cc006edaefde5c98033ddc7e553d027f7b24
Marcos <Marcos@PROFESOR-A9>
2025-03-31 09:46:07 +0200

    PROYECTO CREADO Y CONFIGURADO
    - aplication.properties configurado

---
+++ src/main/java/com/marcosd/orientacion/MarcosdExtraOrientacionApplication.java
@@ -0,0 +1,13 @@
+package com.marcosd.orientacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcosdExtraOrientacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcosdExtraOrientacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosd/orientacion/MarcosdExtraOrientacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosd.orientacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcosdExtraOrientacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
